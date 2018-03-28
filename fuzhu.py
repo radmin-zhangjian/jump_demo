f1968543fad4e18a85260bc29a63a65c215d9b1f
radmin <6204000@qq.comsssss>
2018-03-28 17:40:07 +0800

    PIL组件

--- fuzhu.py
+++ fuzhu.py
@@ -4,8 +4,14 @@
 __auth__ = 'YU'
 
 import os
+import PIL, numpy
+import matplotlib.pyplot as plt
 
 def get_screen_image():
-    os.system('adb shell screencap -p /sdcard/screen.png')
-    
-get_screen_image()
+    os.system('adb shell screencap -p /sdcard/screen.png') #获取手机当前界面的实时截图
+    os.system('adb pull /sdcard/screen.png') #下载当前截图到电脑当前文件夹下
+    numpy.array(PIL.Image.open('screen.png')) #把打开的文件转换成多维数组
+
+figure = plt.figure() #创建一个空白的图片对象
+axes_image = plt.imshow(get_screen_image(), animated=True) #把获取的图片画在坐标轴上
+plt.show()
